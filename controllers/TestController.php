802fa1b81acf561dc00e0d95e9d0ae128defc07b
Zane <push_over@163.com>
2018-09-12 08:45:29 +0800

    过滤机制

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -9,4 +9,31 @@ class TestController
         $log = new \libs\Log('email');
         $log->log('发表成功');
     }
+
+    public function testPurify()
+    {
+        // 1. 生成配置对象
+        $config = \HTMLPurifier_Config::createDefault();
+
+        // 2. 配置
+        // 设置编码
+        $config->set('Core.Encoding', 'utf-8');
+        $config->set('HTML.Doctype', 'HTML 4.01 Transitional');
+        // 设置缓存目录
+        $config->set('Cache.SerializerPath', ROOT . 'cache');
+        // 设置允许的 HTML 标签
+        $config->set('HTML.Allowed', 'div,b,strong,i,em,a[href|title],ul,ol,ol[start],li,p[style],br,span[style],img[width|height|alt|src],*[style|class],pre,hr,code,h2,h3,h4,h5,h6,blockquote,del,table,thead,tbody,tr,th,td');
+        // 设置允许的 CSS
+        $config->set('CSS.AllowedProperties', 'font,font-size,font-weight,font-style,margin,width,height,font-family,text-decoration,padding-left,color,background-color,text-align');
+        // 设置是否自动添加 P 标签
+        $config->set('AutoFormat.AutoParagraph', true);
+        // 设置是否删除空标签
+        $config->set('AutoFormat.RemoveEmpty', true);
+
+        // 3. 过滤
+        // 创建对象
+        $purifier = new \HTMLPurifier($config);
+        // 过滤
+        $clean_html = $purifier->purify($content);
+    }
 }
